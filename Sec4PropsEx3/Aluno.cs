a85ab339c3a53864b177cbb686168dd7a5559872
Vinícius Aquino <vinicius.aquino@estudante.ufcg.edu.br>
2024-10-11 20:17:11 -0300

    Add files via upload

---
+++ Sec4PropsEx3/Aluno.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sec4PropsEx3 {
+    internal class Aluno {
+        public string Nome;
+        public double Nota1, Nota2, Nota3;
+
+        public double NotaFinal() {
+            return Nota1 + Nota2 + Nota3;
+        }
+        public string Situacao() {
+            if (NotaFinal() >= 60) {
+                return "Aprovado";
+            }
+            else {
+                return "Reprovado\nFaltaram " + (60 - NotaFinal()) + " pontos";
+            }
+        }
+    }
+}
---
+++ Sec4PropsEx3/Program.cs
@@ -0,0 +1,27 @@
+﻿using Sec4PropsEx3;
+using System;
+using System.Globalization;
+
+namespace Projeto {
+    internal class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("-=--=--=--=--=--=--=-");
+            Console.WriteLine("VERIFICADOR DE ALUNO");
+            Console.WriteLine("-=--=--=--=--=--=--=-");
+
+            Aluno a = new Aluno();
+            Console.Write("Nome do aluno: ");
+            a.Nome = Console.ReadLine();
+            Console.Write("Nota 1: ");
+            a.Nota1 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Nota 2: ");
+            a.Nota2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Nota 3: ");
+            a.Nota3 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("NOTA FINAL = " + a.NotaFinal());
+
+            Console.WriteLine("SITUAÇÃO: " + a.Situacao());
+        }
+    }
+}
